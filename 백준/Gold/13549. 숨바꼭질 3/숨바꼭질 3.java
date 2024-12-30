d77d14dc067b2f29e3d6280682add42b30846d1b
honi20 <81567670+honi20@users.noreply.github.com>
2024-12-30 17:02:29 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 17224 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,3 +1,8 @@
+/**
+ * [이동 조건]
+ * 1. 수빈이의 위치가 X일 때, 1초 후에 X-1 또는 X+1로 이동한다.
+ * 2. 순간이동을 하는 경우, 0초 후에 2*X 위치로 이동한다.
+ */
 import java.io.*;
 import java.util.*;
 
@@ -5,65 +10,83 @@ public class Main {
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
-
-	static class Node implements Comparable<Node> {
-		int position;
-		int cnt;
+	
+	static class Point implements Comparable<Point> {
+		int pos;
+		int time;
 		
-		public Node(int position, int cnt) {
-			this.position = position;
-			this.cnt = cnt;
+		public Point(int pos, int time) {
+			this.pos = pos;
+			this.time = time;
 		}
 		
-		public int compareTo(Node n) {
-			return this.cnt - n.cnt;
+		@Override
+		public int compareTo(Point p) {
+			return this.time - p.time;
 		}
 	}
+
+	static final int MAX = 100_000;
 	
-	static final int MAX = 100001;
-	
-	static int start;
-	static int end;
-	static int[] dp;
-	static PriorityQueue<Node> queue;
+	static int startPos, endPos;
+	static int result;
 	
 	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		
 		st = new StringTokenizer(br.readLine().trim());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
+		startPos = Integer.parseInt(st.nextToken());
+		endPos = Integer.parseInt(st.nextToken());
 		
-		dp = new int[MAX];
-		Arrays.fill(dp, MAX);
+		solve();
 		
-		queue = new PriorityQueue<>();
-		queue.offer(new Node(start, 0));
-		
-		while (!queue.isEmpty()) {
-			Node cur = queue.poll();
-			int curPos = cur.position;
-			int curCnt = cur.cnt;
+		System.out.println(result);
+	}
 	
-			if (curPos == end) {
-				System.out.println(curCnt);
+	private static void solve() {
+		PriorityQueue<Point> queue = new PriorityQueue<>();
+		int[] minTime = new int[MAX+1];
+		
+		Arrays.fill(minTime, Integer.MAX_VALUE);
+		result = Integer.MAX_VALUE;
+		
+		// 수빈이의 위치를 queue에 넣는다.
+		queue.offer(new Point(startPos, 0));
+		
+		// 빈 큐가 될 때까지 탐색
+		while (!queue.isEmpty()) {
+			Point point = queue.poll();
+			int curPos = point.pos;
+			int curTime = point.time;
+			
+			// 동생의 위치에 도착한 경우, 결과값 갱신 후 탐색 종료
+			if (curPos == endPos) { 
+				result = curTime;
 				break;
 			}
 			
-			if (curPos > 0 && dp[curPos-1] > curCnt + 1) {
-				dp[curPos-1] = curCnt + 1;
-				queue.offer(new Node(curPos-1, curCnt+1));
+			int nextPos;
+			
+			// 순간이동
+			nextPos = curPos * 2;
+			if (nextPos > 0 && nextPos <= MAX && minTime[nextPos] > curTime) {
+				minTime[nextPos] = curTime;
+				queue.add(new Point(nextPos, curTime));
 			}
 			
-			if (curPos < MAX-1 && dp[curPos+1] > curCnt + 1) {
-				dp[curPos+1] = curCnt + 1;
-				queue.offer(new Node(curPos+1, curCnt+1));
+			// +1 이동
+			nextPos = curPos + 1;
+			if (nextPos <= MAX && minTime[nextPos] > curTime + 1) {
+				minTime[nextPos] = curTime + 1;
+				queue.add(new Point(nextPos, curTime+1));
 			}
 			
-			if (curPos > 0 && curPos*2 < MAX && dp[curPos*2] > curCnt) {
-				dp[curPos*2] = curCnt;
-				queue.offer(new Node(curPos*2, curCnt));
+			// -1 이동
+			nextPos = curPos - 1;
+			if (nextPos >= 0 && minTime[nextPos] > curTime + 1) {
+				minTime[nextPos] = curTime + 1;
+				queue.add(new Point(nextPos, curTime+1));
 			}
 		}
 	}
