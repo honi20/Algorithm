6f07b56ef912a06ef050648b161a31b09a050b79
honi20 <81567670+honi20@users.noreply.github.com>
2025-02-14 18:03:41 +0900

    [Platinum V] Title: 사탕상자, Time: 472 ms, Memory: 49520 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.java
+++ 백준/Platinum/2243. 사탕상자/사탕상자.java
@@ -1,3 +1,4 @@
+
 /**
  * [조건]
  * - 명령 횟수: 1 ~ 10^5
@@ -10,9 +11,9 @@ public class Main {
 	static BufferedReader br;
 	static StringBuilder sb;
 	static StringTokenizer st;
-	
+
 	static final int MAX = 1_000_000;
-	
+
 	static int height;
 	static int inputCnt;
 	static int[] tree;
@@ -20,65 +21,69 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		
+
 		init();
-		
+
 		inputCnt = Integer.parseInt(br.readLine().trim());
 		while (inputCnt-- > 0) {
 			st = new StringTokenizer(br.readLine().trim());
 			int oper = Integer.parseInt(st.nextToken());
-			
+
 			if (oper == 1) {
-				int value = Integer.parseInt(st.nextToken());
-				
-				int ans = query(1, 1, MAX, value);
+				int rank = Integer.parseInt(st.nextToken());
+
+				int ans = query(1, 1, MAX, rank);
 				update(1, 1, MAX, ans, -1);
-				
+
 				sb.append(ans).append("\n");
 			}
 			else if (oper == 2) {
 				int index = Integer.parseInt(st.nextToken());
 				int value = Integer.parseInt(st.nextToken());
-				
+
 				update(1, 1, MAX, index, value);
 			}
 		}
 		System.out.println(sb);
 	}
-	
+
 	private static int query(int node, int start, int end, int rank) {
-		// 조건 범위 안에 속하는지 확인
-		if (rank <= 0 || tree[node] < rank) {
-			return -1;
-		}
-		
 		// 순위에 해당하는 사탕 번호 반환
 		if (start == end) {
 			return start;
 		}
 		
 		int mid = (start + end) / 2;
-		return Math.max(query(node*2, start, mid, rank), query(node*2+1, mid+1, end, rank - tree[node*2]));
+		
+		// 왼쪽 자식 서브트리에 속하는 경우
+		if (tree[node*2] >= rank) {
+			return query(node * 2, start, mid, rank);
+		}
+		
+		// 오른쪽 자식 서브트리에 속하는 경우
+		else {
+			return query(node * 2 + 1, mid + 1, end, rank - tree[node * 2]);
+		}
 	}
-	
+
 	private static void update(int node, int start, int end, int index, int value) {
 		if (index > end || index < start) {
 			return;
 		}
-		
+
 		tree[node] += value;
-		
+
 		if (start != end) {
 			int mid = (start + end) / 2;
-			update(node*2, start, mid, index, value);
-			update(node*2+1, mid+1, end, index, value);
+			update(node * 2, start, mid, index, value);
+			update(node * 2 + 1, mid + 1, end, index, value);
 		}
 	}
-	
+
 	// 트리 배열 초기화
 	private static void init() {
-		height = (int)Math.ceil(Math.log(MAX) / Math.log(2));
-		tree = new int[1 << (height+1)];
+		height = (int) Math.ceil(Math.log(MAX) / Math.log(2));
+		tree = new int[1 << (height + 1)];
 		Arrays.fill(tree, 0);
 	}
 }
