cf152bae31102c1bb871d2d652c0b299dae98f47
honi20 <81567670+honi20@users.noreply.github.com>
2025-08-11 23:05:47 +0900

    [Silver V] Title: 올림픽, Time: 132 ms, Memory: 14716 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -74,7 +74,7 @@ public class Main {
                 }
                 else {
                     curScore += cnt;
-                    cnt = 0;
+                    cnt = 1;
                 }
             }
 
